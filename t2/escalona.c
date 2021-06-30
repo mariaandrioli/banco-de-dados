9a715a799b840ec2c1286c53b237d4572d4b7f11
maria <mariakandrioli@gmail.com>
2021-06-30 14:52:43 -0300

    initial files

---
+++ t2/escalona.c
@@ -0,0 +1,10 @@
+#include <strings.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "utils.h"
+
+int main() {
+	return(0);
+}
---
+++ t2/utils.h
@@ -0,0 +1,7 @@
+#ifndef __UTILS__
+#define __UTILS__
+#include <stdint.h>
+
+
+
+#endif
